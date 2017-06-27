b4f9f5c52d1fed84d1b7a875a5c90f60efc9680c
prakash ponali <p.ponali@gmail.com>
2017-06-27 05:56:21 +0530

    Getting table data.

--- DemoSite-broadleaf-5.0.1-GA/utility/src/main/java/com/sptools/jdbc/TableData.java
+++ DemoSite-broadleaf-5.0.1-GA/utility/src/main/java/com/sptools/jdbc/TableData.java
@@ -1,80 +1,61 @@
 package com.sptools.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Vector;
 
 public class TableData {
 
-	public static void main(String[] args) {
-		Connection connection;
+	int totalNuberofTables = 0;
+	
+	public void getTableData(Connection connection, String tableName) {
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sptools_web", "root", "admin");
-			Class.forName("com.mysql.jdbc.Driver");
-
-			TableData data = new TableData();
-			data.getTableData(connection);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public void getTableData(Connection connection) {
-		try {
-
-			System.out.println("Table Data");
+			totalNuberofTables++;
+			System.out.println("Table Data " + tableName);
 			Vector<String> columnNames = new Vector<String>();
 			Statement createStatement = connection.createStatement();
-			ResultSet executeQuery = createStatement.executeQuery("select * from sptools_web.BLC_ADDRESS;");
+			ResultSet executeQuery = createStatement.executeQuery("select * from " + tableName);
 			ResultSetMetaData metaData = executeQuery.getMetaData();
 			int i = 0;
 			System.out.println(executeQuery.getFetchSize());
-			StringBuilder template = new StringBuilder();
-			template.append("INSERT INTO " + "sptools_web.BLC_ADDRESS" + " (");
-			int k = 0;
-			while (k < metaData.getColumnCount()) {
-				k++;
-				System.out.print(metaData.getColumnName(k) + "\t");
-				columnNames.add(metaData.getColumnName(k));
-			}
-			for (int j = 0; j < columnNames.size(); j++) {
-				if (j == columnNames.size() - 1) {
-					template.append(columnNames.get(j));
-				} else {
-					template.append(columnNames.get(j) + ", ");
+			
+			if(executeQuery.getFetchSize() > 0)
+			{
+				StringBuilder template = new StringBuilder();
+				template.append("INSERT INTO " + "sptools_web." + tableName + " (");
+				int k = 0;
+				while (k < metaData.getColumnCount()) {
+					k++;
+					System.out.print(metaData.getColumnName(k) + "\t");
+					columnNames.add(metaData.getColumnName(k));
 				}
-
-			}
-			template.append(" ) VALUES ( ");
-			while (executeQuery.next()) {
-				System.out.println("In for loop");
-				/*
-				 * while (i < metaData.getColumnCount()) { i++;
-				 * System.out.println(executeQuery.getString(metaData.
-				 * getColumnName(i))); }
-				 */
-				while (i < metaData.getColumnCount()) {
-					i++;
-					if (i == columnNames.size()) {
-						template.append(executeQuery.getString(metaData.getColumnName(i)));
+				for (int j = 0; j < columnNames.size(); j++) {
+					if (j == columnNames.size() - 1) {
+						template.append(columnNames.get(j));
 					} else {
-						template.append(executeQuery.getString(metaData.getColumnName(i)) + ", ");
+						template.append(columnNames.get(j) + ", ");
+					}
+
+				}
+				template.append(" ) VALUES ( ");
+				while (executeQuery.next()) {
+					System.out.println("In for loop");
+					while (i < metaData.getColumnCount()) {
+						i++;
+						if (i == columnNames.size()) {
+							template.append(executeQuery.getString(metaData.getColumnName(i)));
+						} else {
+							template.append(executeQuery.getString(metaData.getColumnName(i)) + ", ");
+						}
 					}
 				}
+				template.append(" )");
+				System.out.println(template.toString());
 			}
-			template.append(" )");
-			System.out.println(template.toString());
+			System.out.println("totalNuberofTables --> " + totalNuberofTables);
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
